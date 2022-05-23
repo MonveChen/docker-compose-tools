5717a9289d5b303a54495a9096d7183611581580
Monve <chenmenghui@lazbao.com>
2022-05-23 15:23:58 +0800

    feat: gitlab-runner

--- gitlab-runner/data/deploy.sh
+++ gitlab-runner/data/deploy.sh
@@ -3,20 +3,28 @@
  # @Author: Monve
  # @Date: 2021-11-16 10:34:51
  # @LastEditors: Monve
- # @LastEditTime: 2021-12-31 17:31:02
- # @FilePath: /docker-compose-tools/gitlab-runner/data/deploy.sh
+ # @LastEditTime: 2022-03-01 11:45:15
+ # @FilePath: /ip-proxy-server/Users/chenmenghui/Git/docker-compose-tools/gitlab-runner/data/deploy.sh
 ### 
 
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo $DATE
 
-ssh $SSH_USER@$SSH_IP "if [ ! -f \"$PROJECT_PATH\/$CI_PROJECT_NAME\" ]; then 
-  mkdir $PROJECT_PATH\/$CI_PROJECT_NAME
+PROJECT_ROOT_PATH=$PROJECT_PATH
+if [ "$MULTI_PROJECT" = "true" ]; then
+  PROJECT_ROOT_PATH=$PROJECT_PATH/${CI_PROJECT_NAME%%-*}
+  ssh $SSH_USER@$SSH_IP "if [ ! -f \"$PROJECT_ROOT_PATH\" ]; then 
+    mkdir $PROJECT_ROOT_PATH
+  fi;"
+fi;
+
+ssh $SSH_USER@$SSH_IP "if [ ! -f \"$PROJECT_ROOT_PATH\/$CI_PROJECT_NAME\" ]; then 
+  mkdir $PROJECT_ROOT_PATH\/$CI_PROJECT_NAME
 fi;"
 
-scp -r ./docker-compose.yml $SSH_USER@$SSH_IP:$PROJECT_PATH/$CI_PROJECT_NAME/docker-compose.yml
+scp -r ./docker-compose.yml $SSH_USER@$SSH_IP:$PROJECT_ROOT_PATH/$CI_PROJECT_NAME/docker-compose.yml
 
-ssh $SSH_USER@$SSH_IP "cd $PROJECT_PATH/$CI_PROJECT_NAME \
+ssh $SSH_USER@$SSH_IP "cd $PROJECT_ROOT_PATH/$CI_PROJECT_NAME \
   && echo $DATE >> auto_lunch_log.txt \
   && docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD  $DOCKER_REGISTRY \
   && docker-compose up -d >> auto_lunch_log.txt \
